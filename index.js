45acb8c6ae4bc03f855cacafdd3f036d768f1e4a
SVarshiniBhat <167081191+SVarshiniBhat@users.noreply.github.com>
2024-11-20 23:13:40 +0530

    Add files via upload
    Added node.js server files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
@@ -35,4 +36,20 @@ app.get('/index.html', (req, res) => {        //get requests to the root ("/") w
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
+=======
+const app = require('express')();
+
+const PORT =8080;
+
+app.listen(
+    PORT,
+    () => console.log(`rver running on ${PORT}`)
+);
+
+app.get('/fruits', (req, res)=> {
+    res.send({
+        apple: 2,
+        banana: 12
+    })
+>>>>>>> f392ab7 (Added Node.js server files)
 });
